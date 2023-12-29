ea93d2c7c8eeba9443fb861310a98340943470d3
K Sai Rajesh <sairajesh.korla@navajna.com>
2023-12-29 17:34:13 +0530

    29 dec, 2023 api logger update

--- src/app/api/home/route.js
+++ src/app/api/home/route.js
@@ -1,6 +1,15 @@
 import { NextResponse } from "next/server"
+import logger from "../../../../logger/logger"
 
-export async function GET(request) {
-    console.log(request)
-    return NextResponse.json({ name: 'sai' })
+export async function GET(request, response) {
+    logger.info(`Request headers: ${JSON.stringify(request.headers)}`)
+    try {
+        throw new Error('Oops there was an error .')
+        // return NextResponse.json({ name: 'sai' })
+    } catch (error) {
+        logger.error(error.stack)
+        return NextResponse.error({ msg: 'Error !' })
+    }
+    // return NextResponse.error({msg:})
+    // return response.status(200).json({ msg: 'data recieved !' })
 }
---
+++ src/app/products/page.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function page() {
+    return (
+        <div style={{ padding: '20px' }}>
+            <h1>List of the Products</h1>
+            <ol>
+                <li>product 1</li>
+                <li>product 2</li>
+                <li>product 3</li>
+                <li>product 4</li>
+            </ol>
+        </div>
+    )
+}
+
+export default page
